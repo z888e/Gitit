21b64fa4c7e8bad21590a32a35c6ddd7d75d838a
z888e <zoemhrt@gmail.com>
2022-09-27 10:57:33 +0200

    Initial Commit

---
+++ Gitit/GititApp.swift
@@ -0,0 +1,17 @@
+//
+//  GititApp.swift
+//  Gitit
+//
+//  Created by Zoé Hartman on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GititApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
